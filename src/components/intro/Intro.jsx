e8234d570fce21d1eacd6a6083eda09431bf3e07
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-10-14 01:12:38 +0530

    added arrow animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,25 @@
 import "./intro.scss";
-
+import { KeyboardArrowDown } from "@mui/icons-material";
 const Intro = () => {
   return (
     <div className="intro" id="intro">
-      {/* dfs */}
-      {/* {as} */}
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Vinay Sehwag</h1>
+          <h3>
+            Web Developer <span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDown className="icon" />
+        </a>
+      </div>
     </div>
   );
 };
